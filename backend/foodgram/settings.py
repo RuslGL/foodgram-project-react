5c6d1050b9df7570e0311d21a5964ab3695bbe0e
RuslGL <glotovruslann@yandex.ru>
2022-12-20 19:00:07 +0300

    front works

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -76,16 +76,16 @@ DATABASES = {
 }
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': os.getenv('DB_ENGINE'),
-#        'NAME': os.getenv('DB_NAME'),
-#        'USER': os.getenv('POSTGRES_USER'),
-#        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#        'HOST': os.getenv('DB_HOST'),
-#        'PORT': os.getenv('DB_PORT')
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
