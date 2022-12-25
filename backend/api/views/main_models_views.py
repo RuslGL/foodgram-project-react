3abea1a13130b125ac21ce072c6dd313d7bbee5f
RuslGL <glotovruslann@yandex.ru>
2022-12-25 19:36:16 +0300

    image pull

--- backend/api/views/main_models_views.py
+++ backend/api/views/main_models_views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets
+from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
 from recepies.models import Ingredients, Recipe, Tags
